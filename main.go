01bd68cfb10f40f9cc3134e8c82c070714ba200d
Facundo Lerena <faculerena@gmail.com>
2023-02-06 23:13:28 -0300

    removed test examples and commented validationmiddleware

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
-	"github.com/faculerena/goauth/internal/database"
 	"github.com/faculerena/goauth/internal/token"
-	"github.com/faculerena/goauth/private"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,50 +10,7 @@ import (
 var err error
 
 func main() {
-	/*
-		db, err := database.Setup(private.GetConfig())
-		if err != nil {
-			log.Fatal(err)
-		}
-			check, err := database.CheckUser(db)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(check)
 
-	*/
-	/*
-		myToken, err := token.GenerateToken(private.Header(), private.Payload(), private.Secret())
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(myToken)
-
-			isOk, err := token.ValidateToken(myToken, private.Secret())
-
-			fmt.Println(isOk)
-	*/
-	db, err := database.Setup(private.GetConfig())
-	var s []string
-	s = append(s, "perez", "gomez", "lopez")
-	for i, si := range s {
-		h := sha1.New()
-		h.Write([]byte(si))
-		hashencoder := hex.EncodeToString(h.Sum(nil))
-
-		fmt.Println(s[i], hashencoder)
-	}
-	fmt.Println("------------------------")
-
-	database.DeleteUser(db, 4)
-	database.DeleteUser(db, 7)
-	/*
-		id, err := database.AddUser(db, juancito)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id)
-	*/
 	mainRouter := mux.NewRouter()
 
 	authRouter := mainRouter.PathPrefix("/auth").Subrouter()
@@ -66,7 +19,7 @@ func main() {
 
 	authRouter.HandleFunc("/signin", token.SigninHandler)
 
-	authRouter.Use(token.TokenValidationMiddleware)
+	//	authRouter.Use(token.TokenValidationMiddleware) remove comment if you want to test the token received
 
 	server := &http.Server{
 		Addr:    "127.0.0.1:9090",
